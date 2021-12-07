cd5b4ac5c336f6f3f567d381516a9197f7b0c598
jake <jjohnson11096@gmail.com>
2021-12-07 10:23:22 -0700

    cleanup scaling

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,42 +5,46 @@ const Home = () => {
   return (
     <main style={{
       backgroundColor: "white",
-      display:"flex",
-      height: "100vh",
-      backgroundPosition: "center center",
-      backgroundAttachment: "fixed",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
+      /*display:"flex",
+      height: "100vh",*/
       fontFamily: "Adina",
       }}>
 
       <div style={{
         /*backgroundColor:"rgba(0, 0, 0, 0.4)",*/
         padding: 30,
-        display:"flex",
+        /*display:"flex",
         flex: 1,
-        flexDirection: "column",
-        maxWidth: "800px",
+        flexDirection: "column",*/
+        /*maxWidth: "800px",*/
         marginLeft: "auto",
         marginRight: "auto",
       }}>
       <div
         style={{
           /*backgroundColor: "blue",*/
-          display: "flex",
+          /*display: "flex",
           justifyContent: "center",
           alignItems: "center",
           flex: 1,
-          flexDirection: "column",
+          flexDirection: "column",*/
         }}
       >
         <ReactPlayer
-          style={{marginBottom: "50px"}}
-          width={"700px"}
-          height={"700px"}
+          style={{
+            marginBottom: "5%",
+            
+            /*paddingTop:"56.25%",
+            position:"relative"*/
+          }}
+          /*width={"700px"}
+          height={"700px"}*/
+          width={"100%"}
+          height={"100%"}
           playing={true}
           loop={true} 
-          muted 
+          muted
+          playsinline
           url={[
             {src: 'logo_animated.mp4', type: 'video/mp4'},
           ]}

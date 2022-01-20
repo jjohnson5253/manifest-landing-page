bc3f713c0f0f3233c69b3b60dc7321cf1e115ef2
Jake Johnson <jake.e.johnson@medtronic.com>
2022-01-19 23:45:34 -0700

    cleanup

--- src/LootTest.js
+++ src/LootTest.js
@@ -1,3 +1,9 @@
+//
+// File Description: Implements a loot bag test. You can enter ETH wallet and it will tell you
+// token ids of loot bags owned at that address.
+//
+// Copyright Manifest 2022
+//
 import "./Home.css";
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
@@ -5,13 +11,6 @@ import axios from 'axios';
 const LootTest = () => {
     const [walletAddress, setWalletAddress] = useState("");
     const [nfts,setNfts]=useState([])
-
-    /*useEffect(() => {
-        fetchNfts('0xd82724a01bce06bbc3face916bd14b5b06ef59bd');
-      }, [])
-    useEffect(() => {
-    console.log(nfts)
-    }, [nfts])*/
 
     // brief    async function to get Loot Bag NFTs per given address
     // param    [in] walletAddress: wallet to read from
@@ -42,70 +41,26 @@ const LootTest = () => {
   }
 
   return (
-    <div style={{
-        height:"100vh",
-        width:"100vw",
-        backgroundColor:"gray",
-    }}
-    >
-        <div style={{
-            paddingTop:"10px",
-            textAlign:"center",
-            marginBottom:"10px"
-        }}>
-            <p style={{
-              marginBottom:"20px",
-            }}>
-              Loot Test
-            </p>
+    <div style={{height:"100vh",width:"100vw",backgroundColor:"gray",}}>
+        <div style={{paddingTop:"10px",textAlign:"center",marginBottom:"10px"}}>
+          <p className="testLootHeader" style={{marginBottom:"20px", fontSize:"40px",}}>
+            Loot Test
+          </p>
 
-        <input type="text" onChange={getWalletAddress} placeholder="Enter wallet address..." id="myInput"></input>
+          <input type="text" onChange={getWalletAddress} placeholder="Enter wallet address..." id="myInput"></input>
+          <button type="button" onClick={buttonClick}>Go</button>
 
-        <button type="button" onClick={buttonClick}>Go</button>
-
-        <p className="testLootHeader" style={{
-              fontSize:"20px",
-            }}>
-              Loot Bags Owned:
-        </p>
-
+          <p className="testLootHeader" style={{fontSize:"20px"}}>
+            Loot Bags Owned:
+          </p>
         </div>
 
-    {/*<div>
-      {
-        comments && comments.map(comment=>{
-          return(
-            <div key={comment} style={{alignItems:'center',margin:'20px 60px'}}>
-            <h4>{}</h4>
-            <p>{}</p>
-          </div>
-          )
-
-        })
-      }
-    </div>*/}
-
-        {/*<p className="testLootHeader">
-            LootTest
-        </p>*/}
-
-        
         {nfts.map((nft, index) => {
           return (
             <div key={index} style={{ marginTop: "0px", padding: "8px" }}>
               <div className="testLootHeader" >Token Id: {nft.token_id}</div>
             </div>)
         })} 
-        
-
-        {/*{nfts.map((tokenId, index) => {
-          return (
-            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
-              <div>Address: {tokenId.address}</div>
-              <div>Time: {tokenId.timestamp.toString()}</div>
-              <div>Message: {tokenId.message}</div>
-            </div>)
-        })}*/}
     </div>
   );
 };
